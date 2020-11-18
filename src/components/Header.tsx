b97e4cec55e40d5d913c7ffea9ed938f9710cf7d
yeshwant <yeshwant@example.com>
2020-11-19 00:31:48 +0530

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import Weather from '../Features/Weather/Weather';
+import HeartBeat from '../Features/Heartbeat/heartBeat';
+
+const useStyles = makeStyles({
+    grow: {
+        flexGrow: 1,
+    },
+});
+
+export default () => {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" color="inherit" className={classes.grow}>
+                    yeshwanth's React EOGResource Assessment
+                </Typography>
+                <HeartBeat />
+                <Weather />
+            </Toolbar>
+        </AppBar>
+    );
+};
